08657df8a8b468abafa3f1eae87f16794f9d34c5
ansj <ansj-sun@163.com>
2020-04-12 17:35:47 +0800

    Update repos.cmake
    make PROTOBUF_URL to Latest address

--- cmake/external/github/repos.cmake
+++ cmake/external/github/repos.cmake
@@ -1,3 +1,3 @@
 SET(ASIO_URL https://github.com/chriskohlhoff/asio.git)
-SET(PROTOBUF_URL https://github.com/google/protobuf.git)
+SET(PROTOBUF_URL https://github.com/protocolbuffers/protobuf.git)
 SET(GTEST_URL https://github.com/google/googletest.git)
